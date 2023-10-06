ebc047c5022af945ab8ce0f18e468e44de06bc32
Jddg95 <140569072+Jddg95@users.noreply.github.com>
2023-10-06 03:55:38 -0500

    Update generateReadme function

---
+++ develop/README.md
@@ -0,0 +1,43 @@
+
+  # pp
+  
+  ## Description
+  
+  p
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  p
+  
+  ## Usage
+  
+  p
+  
+  ## License
+  
+  This project is licensed under the p license.
+  
+  ## Contributing
+  
+  undefined
+  
+  ## Tests
+  
+  undefined
+  
+  ## Questions
+  
+  For any questions or inquiries, please reach out to me:
+  
+  - **GitHub**: [p](https://github.com/p)
+  - **Email**: [pp](mailto:pp)
+    
--- develop/index.js
+++ develop/index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
@@ -39,25 +40,81 @@ const questions = [
       },
     ];
 
-// using inquirer.js to prompt the user for information 
-inquirer.promp(questions).then((answers) => {
-    // the userts answers are storesd in the answers 
-    console.log(`User input`, answers);
-});
+
+// Function to generate README content based on user input
+function generateReadme(answers) {
+    // Create a template string for the README content
+    const readmeContent = `
+  # ${answers.project_title}
+  
+  ## Description
+  
+  ${answers.project_description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${answers.installation_instructions}
+  
+  ## Usage
+  
+  ${answers.usage_instructions}
+  
+  ## License
+  
+  This project is licensed under the ${answers.license_name} license.
+  
+  ## Contributing
+  
+  ${answers.contribution_guidelines}
+  
+  ## Tests
+  
+  ${answers.test_instructions}
+  
+  ## Questions
+  
+  For any questions or inquiries, please reach out to me:
+  
+  - **GitHub**: [${answers.your_name}](https://github.com/${answers.your_name})
+  - **Email**: [${answers.your_email}](mailto:${answers.your_email})
+    `;
+  
+    // Return the generated README content as a string
+    return readmeContent;
+  }
+  
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    try {
     fs.writeFileSync(fileName, data);
-}
+    console.log(`${fileName} has been generated successfully!`);
+} catch (error) {
+    console.error(`An error occured while writing ${fileName}:`, error);
+}}
 
 
 // TODO: Create a function to initialize app
 function init() {
-    //Use Inquirer.js to prompt the user for input 
-    inquirer.prompt(questions).then((answers) =>{
-        //Create README content using the provided information 
+    // Use Inquirer.js to prompt the user for input 
+    inquirer.prompt(questions).then((answers) => {
+        // Create README content using the provided information 
         const readmeContent = generateReadme(answers);
-    })
+        // Specify the file name where you want to save the README
+        const fileName = 'README.md';
+        // Write the README.md file using the writeToFile function (corrected function name)
+        writeToFile(fileName, readmeContent);
+    });
 }
 
 
