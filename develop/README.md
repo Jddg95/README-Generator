6ebd5644e79367820a1df783d3121c2367b8a13b
Jddg95 <140569072+Jddg95@users.noreply.github.com>
2023-10-09 19:44:55 -0500

    added mit badge

---
+++ develop/README.md
@@ -0,0 +1,35 @@
+
+  
+  ## Title 
+
+  
+  
+  ## Description
+  
+  
+  
+  ## Installation
+  
+  
+  
+  ## Usage
+  
+  
+  
+  ## License
+  
+   
+
+  
+  ## Tests
+  
+  
+  
+  ## Questions
+  
+  For any questions or inquiries, please reach out to me:
+  
+  - **GitHub**: [](https://github.com/)
+  - **Email**: [](mailto:)
+  
+    
---
+++ develop/utils/readmeTemplate.js
@@ -0,0 +1,32 @@
+const readmeTemplate = `
+# Project Title
+
+## Description: 
+
+## License Badge
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Installation
+
+...
+
+## Usage
+
+...
+
+## License
+
+...
+
+## Tests
+
+...
+
+## Questions
+
+GitHub: [GitHubUsername](https://github.com/GitHubUsername)
+Email: [Email](mailto:youremail@example.com)
+`;
+
+module.exports = readmeTemplate;
